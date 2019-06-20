3f7921abe245b68e802a27542b0fa7a52ba64275
Oriental Fantasy <1684008344@qq.com>
2019-06-20 16:52:15 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,11 +1,11 @@
 <?php
 /**
-* 东方幻梦 2019-3-22
-* https://blog.dfhm.me/
+* 东方幻梦 2019-6-20
+* https://blog.mxlbs.cn/
 * 该程序使用了百度AI的SDK
 * 该程序使用 GPL V3.0 协议
 **/
 ob_start();
-require_once 'dfhm.php';
+require_once 'config.php';
 header("Content-type:audio/mp3");
 ?>
